c2ea652d63a60a1ccdee22ed0e34d6d8bddc0cf3
Rachit Shrivastava <rshriva@ncsu.edu>
2018-06-29 16:54:10 -0700

    Using homepage link from package.json

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var package = require('./package.json');
+
+var webview = document.createElement("webview");
+webview.id = "portal";
+webview.src = package.homepage;		// assign homepage url from package.json
+webview.style = "height: 600px; width: 800px";
+
+document.body.appendChild(webview);
+
