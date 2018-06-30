23ec3f94cdbe8feaca7bdc3bdc3d67462798f087
Rachit Shrivastava <rshriva@ncsu.edu>
2018-06-29 17:14:54 -0700

    Autosizing the content of <webview>

--- renderer.js
+++ renderer.js
@@ -7,7 +7,11 @@ var package = require('./package.json');
 var webview = document.createElement("webview");
 webview.id = "portal";
 webview.src = package.homepage;		// assign homepage url from package.json
-webview.style = "height: 600px; width: 800px";
+// autosize screen resolution as per window size
+// ref: https://electronjs.org/docs/api/webview-tag#autosize
+webview.autosize = "on";
+webview.minwidth = "800";
+webview.minheight = "600";
 
 document.body.appendChild(webview);
 
